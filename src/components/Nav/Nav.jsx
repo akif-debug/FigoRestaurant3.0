8f517fb79c7a84be26a62cbae7a972f69c101ed0
13.5coder <akifmansoori8287@gmail.com>
2022-05-22 14:43:18 +0530

    NavbarCompleted

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './Nav.css'
 import {Link} from 'react-router-dom'
 import {BsHandbag} from 'react-icons/bs'
 import bag from '../../assets/bag.png'
+import {BiArrowToBottom} from 'react-icons/bi'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Nav = () => {
+	const navRef = useRef();
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav")
+	}
 	return (
-		<>
-				<div className="nav-container">
-			<nav className="nav">
-					<div className="nav-left-side">
-						<Link to='Home' className='nav-brand'>Figo</Link>
-					</div>
-					<div className="nav-right-side">
-						<Link to='Home' className='nav-links'>home</Link>
-						<Link to='Menu' className='nav-links'>menu</Link>
-						<Link to='' className='nav-links'>service</Link>
-						<Link to='' className='nav-links'>sign in</Link>
-						<Link to='' className='nav-links nav-icon'><img src={bag} /></Link>
-					</div>
-			</nav>	
-				</div>
-		</>
+		<header className='nav-header'>
+			<h3 className='nav-brand'>Figo</h3>
+			<nav ref={navRef}>
+				<Link to='Home' className='nav-links'>Home</Link>
+				<Link to='Menu' className='nav-links'>Menu</Link>
+				<Link to='Menu' className='nav-links'>Service</Link>
+				<Link to='Menu' className='nav-links'>Sign in</Link>
+				<Link to='Menu' className='nav-links nav-icon'><img src={bag} /></Link>
+				<button className='nav-btn nav-close-btn' onClick={showNavbar}>
+					<FaTimes/>
+				</button>
+			</nav>
+			<button className='nav-btn' onClick={showNavbar}>
+				<FaBars/>
+			</button>
+		</header>
 	)
 }
 
